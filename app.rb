324faf904fdd5411ee83d272357ed21bf94aef5b
r-plus <anasthasia.r@gmail.com>
2014-05-09 11:35:31 +0900

    add importer

--- app.rb
+++ app.rb
@@ -36,7 +36,43 @@ post "/upload" do
   File.open('uploads/' + params['myfile'][:filename], "w") do |f|
     f.write(params['myfile'][:tempfile].read)
   end
-  return "The file was successfully uploaded!"
+  system('iconv -f SHIFT_JIS -t UTF-8 uploads/' + params['myfile'][:filename] + ' > uploads/utf.txt')
+  import_ajs_result
+  redirect '/'
+end
+
+def import_ajs_result
+  client = Elasticsearch::Client.new log: false
+  array = []
+  open("uploads/utf.txt") do |file|
+    while l = file.gets
+      a = l.split()
+      if a.length == 8# && a[1] == 'net'
+        name = a[0]
+        category = a[1]
+        start_str = (a[4] + 'T' + a[5]).gsub("/", "-")
+        end_str = (a[6] + 'T' + a[7]).gsub("/", "-")
+        if start_str !~ /^\*/ && end_str !~ /^\*/
+          start_date = DateTime.parse(start_str)
+          end_date = DateTime.parse(end_str)
+          duration = ((end_date - start_date) * 24 * 60 * 60).to_i
+          array.push({
+              index: {
+                _index: 'ajs',
+                _type: 'result',
+                _id: name + start_str,
+                data: { name: name, category: category, start: start_str, end: end_str, duration: duration, :@timestamp => start_str}
+              }
+          })
+
+          #client.index index: 'ajs', type: 'result', body: { name: name, start: start_str, end: end_str, duration: duration}
+        end
+      end
+    end
+  end
+  if array.length >= 1
+    client.bulk body: array
+  end
 end
 
 __END__
